ca351f628fef6a3f51dfac08565ba46484e571a7
Fran Dieguez <fran.dieguez@mabishu.com>
2016-09-09 11:43:56 +0200

    Improve git-things-in-develop
    Signed-off-by: Fran Dieguez <fran.dieguez@mabishu.com>

--- shell/config/git.sh
+++ shell/config/git.sh
@@ -7,6 +7,6 @@ alias undopush="git push -f origin HEAD^:master"
 
 # git root
 alias gr='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
-alias git-things-in-develop="git log develop ^master --no-merges"
+alias git-things-in-develop="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative master..develop --no-merges"
 
 alias release="git flow release start $(date +%Y%m%d)"
