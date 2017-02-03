5ad8ff201dacee7e80434e92cac05fb16ab39bea
Fran Dieguez <fran.dieguez@mabishu.com>
2017-02-03 12:27:50 +0100

    Add android tools and platform-tools path to PATH

--- shell/config/android.sh
+++ shell/config/android.sh
@@ -1,3 +1,6 @@
 if [ -e $HOME/.android-sdk-linux ]; then
   export ANDROID_HOME=$HOME/.android-sdk-linux
+  export PATH=$PATH:$ANDROID_HOME/tools
+  export PATH=$PATH:$ANDROID_HOME/platform-tools  
 fi
+
