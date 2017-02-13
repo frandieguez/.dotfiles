7e9eb3b498b605fe11d53db979fba0532af07867
Fran Dieguez <fran.dieguez@mabishu.com>
2017-02-13 17:51:09 +0100

    Move ~/bin path inclusion to shell/config/path.sh

--- shell/config/misc.sh
+++ shell/config/misc.sh
@@ -1,6 +1,1 @@
-# Path variables
-if [ -e $HOME/bin ]; then
-  export PATH=$PATH:$HOME/bin
-fi
-
 JIRA_RAPID_BOARD=true
--- shell/config/path.sh
+++ shell/config/path.sh
@@ -1,3 +1,8 @@
+# Path variables
 if [ -d /usr/share/bin ]; then
     export PATH=/usr/share/bin:$PATH
+fi
+
+if [ -e $HOME/bin ]; then
+  export PATH=$PATH:$HOME/bin
 fi
