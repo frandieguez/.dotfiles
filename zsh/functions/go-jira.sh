e85a29466a94bb002dce4ef57a78266569a08b18
Fran Dieguez <fran.dieguez@mabishu.com>
2017-11-24 17:19:45 +0100

    Fix path to template after go-jira update

--- zsh/functions/go-jira.sh
+++ zsh/functions/go-jira.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# ---
+# Check an issue description and mark all tickets included.
+#
+# @param $1 The issue key.
+# ---
 function jira_done_all() {
     tickets=`jira $1 | sed -e "/.*\(\(ONM\|OT\)-[0-9]\+\).*/!d" -e "s/.*\(\(ONM\|OT\)-[0-9]\+\).*/\1/g" | sort | uniq`
 
@@ -8,6 +13,12 @@ function jira_done_all() {
     done
 }
 
+# ---
+# Extracts a field from an issue.
+#
+# @param $1 The issue key.
+# @param $2 The field to extract.
+# ---
 function jira_extract() {
     field=`jira $1 | ag ^$2: | sed -e "s/$2:\s\+//g"`
 
@@ -15,10 +26,20 @@ function jira_extract() {
     echo $1 $field | xclip -selection c
 }
 
+# ---
+# Extract a summary from an issue.
+#
+# @param $1 The issue key.
+# ---
 function jira_extract_summary() {
     jira_extract $1 summary
 }
 
+# ---
+# Searches issues and copies the result to the clipboard.
+#
+# @param $1 The criteria to search by.
+# ---
 function jira_list_and_extract() {
     tickets=`jira list -q "$1" | sed -e "s/:\s\+/ /g"`
 
@@ -26,7 +47,13 @@ function jira_list_and_extract() {
     echo $tickets | xclip -selection c
 }
 
+# ---
+# Adds a worklog to an issue.
+#
+# @param $1 The worklog information as "starttime|endtime|time|key|project"
+# ---
 function jira_log_activity() {
+    echo "activity: $1"
     id=`echo $1 | cut -d'|' -f4`
     comment=`echo $1 | cut -d'|' -f6`
 
@@ -37,30 +64,41 @@ function jira_log_activity() {
 
     echo "$id - $date - $duration - $comment"
 
-    echo "comment: |~" > /tmp/jira-template.tmp;
-    echo "    $comment" >> /tmp/jira-template.tmp;
-    echo "timeSpent: $duration" >> /tmp/jira-template.tmp;
-    echo "started: $date" >> /tmp/jira-template.tmp;
+    echo "comment: |~" > ~/.jira.d/templates/jira-template.tmp;
+    echo "    $comment" >> ~/.jira.d/templates/jira-template.tmp;
+    echo "timeSpent: $duration" >> ~/.jira.d/templates/jira-template.tmp;
+    echo "started: $date" >> ~/.jira.d/templates/jira-template.tmp;
 
-    jira add worklog $id -t /tmp/jira-template.tmp --noedit;
+    jira worklog add $id -t jira-template.tmp --noedit
 }
 
+# ---
+# Logs all work stored in project hamster for a date to JIRA.
+#
+# @param $1 The date to log.
+# ---
 function jira_log_day() {
     SAVEIFS=$IFS;
     IFS=$(echo -en "\n\b");
 
     activities=`hamster search '' $1 | \
         sed -e ':a' -e 'N' -e '$!ba' -e "s/\n\s\+/|/g" | \
-        tail -n +4 | head -n -3 | sed -e "s/\s\+|\s\+/|/g" | \
-        grep "[A-Z]\+[0-9]\+"`;
+        tail -n +4 | head -n -3 | sed -e "s/\s*|\s*/|/g" | \
+        grep "[A-Z]\+-[0-9]\+"`;
 
     for activity in `echo $activities`; do
-        jira_log_activity "$1 $activity"
+        jira_log_activity $activity
     done
 
     IFS=$SAVEIFS
 }
 
+# ---
+# Logs all work stored in project hamster between two dates.
+#
+# @param $1 The start date.
+# @param $1 The end date.
+# ---
 function jira_log_from_to() {
     SAVEIFS=$IFS;
     IFS=$(echo -en "\n\b");
@@ -78,6 +116,9 @@ function jira_log_from_to() {
     IFS=$SAVEIFS
 }
 
+# ---
+# Logs all work done in the current week.
+# ---
 function jira_log_week() {
     start=`date -d "- $(date +%u) days + 1 day" +%Y-%m-%d`;
     end=`date -d "+ 7 days - $(date +%u) days" +%Y-%m-%d`;
