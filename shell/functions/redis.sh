fefd66857efb6700d57ee474b2ac26c2f81ca3e3
Fran Dieguez <fran.dieguez@mabishu.com>
2018-08-07 09:57:57 +0200

    Move zsh functions to shell functions

--- shell/functions/redis.sh
+++ shell/functions/redis.sh
@@ -1,39 +1,41 @@
-#!/bin/sh
-
 # Get values for the list of keys
-function redis_get() {
-    keys=($(redis_search $1))
-    i=1;
+function redis-get() {
+  keys=($(redis-search $1))
+  i=1;
 
-    for key in $keys; do
-        if [[ $i -gt 1 ]]; then
-            echo ""
-        fi
+  for key in $keys; do
+    if [[ $i -gt 1  ]] then
+      echo "";
+    fi
 
-        echo $i")" $key
-        redis-cli get $key
-        i=$(( $i + 1 ))
-    done;
+    echo $i")" $key
+    redis-cli get $key
+    i=$(( $i + 1  ))
+  done;
+
 }
 
-# List all redis_keys
-function redis_list() {
-    redis-cli keys "*"
+# List all redis-keys
+function redis-list() {
+  redis-cli keys "*"
+
 }
 
 # Search keys by pattern
-function redis_search() {
-    redis_list | grep $1
+function redis-search() {
+  redis-list | grep $1
+
 }
 
 # Delete keys by pattern
-function redis_delete() {
-    redis_search $1 | xargs redis-cli del
+function redis-delete() {
+  redis-search $1 | xargs redis-cli del
+
 }
 
 # Alias
-alias rdd='redis_delete'
+alias rdd='redis-delete'
 alias rdf='redis-cli flushall'
-alias rdg='redis_get'
-alias rdl='redis_list'
-alias rds='redis_search'
+alias rdg='redis-get'
+alias rdl='redis-list'
+alias rds='redis-search'
