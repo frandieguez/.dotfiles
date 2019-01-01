6dafed6d6b47bdf6dff60b15ecfba7b889d306d9
Fran Dieguez <fran.dieguez@mabishu.com>
2019-01-01 23:46:02 +0100

    Clean zsh bindings config

--- src/config/zsh/config/bindings.sh
+++ src/config/zsh/config/bindings.sh
@@ -1,13 +1,10 @@
 # zsh-users/zsh-autosuggestions
-bindkey '^[[C' autosuggest-accept
-bindkey '^ ' autosuggest-accept
 bindkey -M vicmd ' ' autosuggest-accept
 bindkey -M vicmd 'L' autosuggest-accept
 
 # zsh-users/zsh-history-substring-search
 bindkey -M vicmd 'k' history-substring-search-up
 bindkey -M vicmd 'j' history-substring-search-down
-bindkey -a '^[[3~' delete-char
 
 bindkey '^ ' snippet-expand
 bindkey '^F' fuck-command-line
