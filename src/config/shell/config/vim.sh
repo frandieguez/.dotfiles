34ddbc7469acd7b12fba0a831e3c3ab56c2c67ec
Fran Dieguez <fran.dieguez@mabishu.com>
2019-04-26 23:43:51 +0200

    Make nvim detection silent during VIMRUNTIME initialization

--- src/config/shell/config/vim.sh
+++ src/config/shell/config/vim.sh
@@ -1,4 +1,4 @@
 # Set the vim runtime for nvim
-if type nvim; then
+if [ -x "$(command -v nvim)" ]; then
     export VIMRUNTIME=$(find /usr/share -type d -name runtime 2> /dev/null|grep nvim |head -1)
 fi
