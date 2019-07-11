099634ef9709cce6677f2f458651eec95aca4e62
Fran Dieguez <fran.dieguez@situm.es>
2019-07-11 13:40:20 +0200

    Avoid collision b/ vim<->nvim while doing git commits

--- src/config/shell/config/vim.sh
+++ src/config/shell/config/vim.sh
@@ -2,3 +2,9 @@
 if [ -x "$(command -v nvim)" ]; then
     export VIMRUNTIME=$(find /usr/share -type d -name runtime 2> /dev/null|grep nvim |head -1)
 fi
+
+if [[ -n "${NVIM_LISTEN_ADDRESS}" ]]
+then
+    # TODO update the path each time Vim has a major upgrade
+    export VIMRUNTIME=/usr/share/vim/vim81
+fi
