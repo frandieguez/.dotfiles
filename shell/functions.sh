2c92808f10416e3d23322c5ead49e47c791c5048
Fran Dieguez <fran.dieguez@situm.com>
2023-10-24 08:06:30 +0200

    remove custom j function

--- shell/functions.sh
+++ shell/functions.sh
@@ -2,13 +2,13 @@ function cdd() {
   cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
 }
 
-function j() {
-  fname=$(declare -f -F _z)
-
-  [ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
-
-  _z "$1"
-}
+# function j() {
+#   fname=$(declare -f -F _z)
+# 
+#   [ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
+# 
+#   _z "$1"
+# }
 
 function recent_dirs() {
   # This script depends on pushd. It works better with autopush enabled in ZSH
