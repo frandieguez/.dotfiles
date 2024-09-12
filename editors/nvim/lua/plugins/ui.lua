f85c56d67e033e4c060abb2f97034ed37acbe3bd
Fran Dieguez <fran.dieguez@situm.com>
2024-09-12 11:09:52 +0200

    Configure toggleterm

--- editors/nvim/lua/plugins/ui.lua
+++ editors/nvim/lua/plugins/ui.lua
@@ -273,5 +273,20 @@ return {
 			-- end
 		end,
 	},
-	{ "akinsho/toggleterm.nvim", version = "*", config = true },
+	{
+		"akinsho/toggleterm.nvim",
+		config = true,
+		cmd = "ToggleTerm",
+		keys = { { "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
+		opts = {
+			open_mapping = [[<F4>]],
+			direction = "float",
+			shade_filetypes = {},
+			hide_numbers = true,
+			insert_mappings = true,
+			terminal_mappings = true,
+			start_in_insert = true,
+			close_on_exit = true,
+		},
+	},
 }
